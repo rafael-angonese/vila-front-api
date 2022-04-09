9b141d4b46d5ab05d33e606b0f04cd94efb51024
Rafael Angonese <rafael.angonese09@gmail.com>
2022-04-09 18:41:38 -0300

    :sparkles: feat: created show contact route

--- src/routes/contact.routes.ts
+++ src/routes/contact.routes.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express'
 import { CreateContactController } from '../controllers/contact/CreateContactController'
 import { ListContactController } from '../controllers/contact/ListContactController'
+import { ShowContactController } from '../controllers/contact/ShowContactController'
 
 const createController = new CreateContactController()
 const listController = new ListContactController()
+const showController = new ShowContactController()
 
 
 const router = Router()
 
 router.get('/', listController.handle)
 router.post('/', createController.handle)
+router.get('/:id', showController.handle)
 
 export default router
