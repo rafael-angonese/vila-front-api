d745f4d9359793d8f94c15b1be9cede9c5e5cd25
Rafael Angonese <rafael.angonese09@gmail.com>
2022-04-09 18:56:11 -0300

    :sparkles: feat: created delete contact route

--- src/routes/contact.routes.ts
+++ src/routes/contact.routes.ts
@@ -2,16 +2,18 @@ import { Router } from 'express'
 import { CreateContactController } from '../controllers/contact/CreateContactController'
 import { ListContactController } from '../controllers/contact/ListContactController'
 import { ShowContactController } from '../controllers/contact/ShowContactController'
+import { DeleteContactController } from '../controllers/contact/DeleteContactController'
 
 const createController = new CreateContactController()
 const listController = new ListContactController()
 const showController = new ShowContactController()
-
+const deleteController = new DeleteContactController()
 
 const router = Router()
 
 router.get('/', listController.handle)
 router.post('/', createController.handle)
 router.get('/:id', showController.handle)
+router.delete('/:id', deleteController.handle)
 
 export default router
