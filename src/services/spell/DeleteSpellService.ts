6328a169210435862a66c2d249de0357d1156eb0
Rafael Angonese <rafael.angonese09@gmail.com>
2022-04-11 02:16:29 -0300

    :sparkles: feat: created DeleteSpellService

---
+++ src/services/spell/DeleteSpellService.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+import { prismaClient } from "../../database/prismaClient";
+import { NotFoundError } from "../../errors/NotFoundError";
+import { PrismaError } from "../../errors/PrismaError";
+import { ShowSpellService } from "./ShowSpellService";
+
+const showService = new ShowSpellService();
+
+export class DeleteSpellService {
+    async execute(id: string): Promise<PrismaClient['Spell']> {
+
+        const spell = await showService.execute(id)
+
+        if (!spell) {
+            throw new NotFoundError()
+        }
+
+        try {
+            const deleted = await prismaClient.spell.delete({
+                where: { id },
+            })
+
+            return deleted;
+        } catch (error) {
+            throw new PrismaError(error)
+        }
+    }
+}
