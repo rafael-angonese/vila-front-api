69fd64f24d18a79a868b5c38b1e644abcc74927a
Rafael Angonese <rafael.angonese09@gmail.com>
2022-04-11 02:18:01 -0300

    :sparkles: feat: created spell validation schema

---
+++ src/validators/spell.schema.ts
@@ -0,0 +1,11 @@
+import yup from "../config/yup";
+
+const spellSchema = yup.object({
+    name: yup.string().min(1).max(255).required(),
+    element: yup.string().min(1).max(255).required(),
+    spell_word: yup.string().min(1).max(255).required(),
+    mana: yup.number().required(),
+});
+
+
+export default spellSchema
