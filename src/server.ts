424f7ef32c3b43ae76e6b442ab6027a95aa8d57a
Rafael Angonese <rafael.angonese09@gmail.com>
2022-04-11 02:25:48 -0300

    :rocket: chore: use env PORT to deploy

--- src/server.ts
+++ src/server.ts
@@ -27,6 +27,6 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
     });
 });
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
     console.log('Server listening on port: 3333')
 })
