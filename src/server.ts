80c4773913c354a6acf306bf642b2601cd1e20f0
Rafael Angonese <rafael.angonese09@gmail.com>
2022-04-11 00:20:37 -0300

    :wrench: feat: config express async errors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors'
 
 import routes from './routes'
 import swaggerOptions from './config/swagger.conf'
+import AppError from './errors/AppError';
 
 const app = express()
 
@@ -10,6 +12,20 @@ expressSwagger(swaggerOptions)
 
 app.use(express.json())
 app.use(routes)
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json(err);
+    }
+
+    console.log(err)
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+        // message: `Internal server error - ${err.message}`,
+    });
+});
 
 app.listen(3333, () => {
     console.log('Server listening on port: 3333')
